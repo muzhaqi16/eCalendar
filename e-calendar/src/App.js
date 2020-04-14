30787d39c84b219c2dfe24aab326e6cf89708f8f
Artan Muzhaqi <artanmuzhaqi@gmail.com>
2020-04-13 20:45:37 -0400

    implement full calendar working

--- e-calendar/src/App.js
+++ e-calendar/src/App.js
@@ -1,12 +1,26 @@
 import React from 'react';
-import './App.css';
+import FullCalendar from '@fullcalendar/react'
+import dayGridPlugin from '@fullcalendar/daygrid'
+import interactionPlugin from "@fullcalendar/interaction";
+import './main.scss'
 
 function App() {
   return (
     <div className="App">
-
+      <FullCalendar
+        dateClick={handleDateClick}
+        defaultView="dayGridMonth"
+        plugins={[dayGridPlugin, interactionPlugin]}
+        events={[
+          { title: 'event 1', date: '2020-04-01' },
+          { title: 'event 2', date: '2020-04-02' }
+        ]}
+      />
     </div>
   );
+}
+const handleDateClick = (arg) => { // bind with an arrow function
+  alert(arg.dateStr)
 }
 
 export default App;
