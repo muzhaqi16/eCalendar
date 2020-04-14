f451e12a0c00d2a3bab3c41959c684076d12572b
Artan Muzhaqi <artanmuzhaqi@gmail.com>
2020-04-13 20:59:34 -0400

    Change the calendar layout and add events on click

--- e-calendar/src/App.js
+++ e-calendar/src/App.js
@@ -10,6 +10,11 @@ function App() {
       <FullCalendar
         dateClick={handleDateClick}
         defaultView="dayGridMonth"
+        // header={{
+        //   left: 'prev,next today',
+        //   center: 'title',
+        //   right: 'dayGridMonth,timeGridWeek,timeGridDay,listWeek'
+        // }}
         plugins={[dayGridPlugin, interactionPlugin]}
         events={[
           { title: 'event 1', date: '2020-04-01' },
@@ -21,6 +26,15 @@ function App() {
 }
 const handleDateClick = (arg) => { // bind with an arrow function
   alert(arg.dateStr)
+  // if (confirm('Would you like to add an event to ' + arg.dateStr + ' ?')) {
+  //   this.setState({  // add new event data
+  //     calendarEvents: this.state.calendarEvents.concat({ // creates a new array
+  //       title: 'New Event',
+  //       start: arg.date,
+  //       allDay: arg.allDay
+  //     })
+  //   })
+  // }
 }
 
 export default App;
