a371efb074413f76f79cfd2247f3c9d830d81532
Artan Muzhaqi <artanmuzhaqi@gmail.com>
2020-04-13 20:04:03 -0400

    Remove project

--- google-fonts-react/src/config.js
+++
@@ -1,5 +0,0 @@
-export default {
-    API_URL: "https://www.googleapis.com/webfonts/v1/webfonts?",
-    FONT_URL: "https://fonts.googleapis.com/css?family=",
-    API_KEY: 'AIzaSyCR74BHJG1r8cfyJQ9_fgsf-OreONRBb8s'
-}
