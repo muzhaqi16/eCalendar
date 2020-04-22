8f474ac2072c02d6f8197a182ac01e69fea87fce
Artan Muzhaqi <artanmuzhaqi@gmail.com>
2020-04-22 19:41:05 -0400

    Fully working add event modal

--- e-calendar/src/components/EventModal/EventModal.js
+++ e-calendar/src/components/EventModal/EventModal.js
@@ -1,11 +1,30 @@
 import React, { Component } from 'react'
 import { Modal, Button, Form, Row, Col } from 'react-bootstrap';
 // there is a findDomNode warning that needs to be fixed
+import * as moment from 'moment';
 
 class EventModal extends Component {
-    handleAddEvent = (event) => {
-        event.preventDefault();
-        console.log(event.target.formTitle.value);
+    handleAddEvent = (e) => {
+        e.preventDefault();
+        const title = e.target.eventTitle.value;
+        const start = moment(e.target.startDate.value).format('YYYY-MM-DD');
+        const end = moment(e.target.endDate.value).format('YYYY-MM-DD');
+        const startTime = e.target.beginHour.value;
+        const endTime = e.target.endHour.value;
+        const people = e.target.people.value;
+        const location = e.target.location.value;
+        const description = e.target.location.value;
+        const event = {
+            title,
+            start,
+            end,
+            startTime,
+            endTime,
+            people,
+            location,
+            description
+        }
+        this.props.addEvent(event);
         this.props.handleClose();
     }
     render() {
@@ -20,36 +39,36 @@ class EventModal extends Component {
                     </Modal.Header>
                     <Modal.Body>
                         <Form onSubmit={this.handleAddEvent}>
-                            <Form.Group as={Row} controlId="formTitle">
+                            <Form.Group as={Row} controlId="eventTitle">
                                 <Form.Label column sm="2">Title</Form.Label>
                                 <Col sm="10">
-                                    <Form.Control type="text" placeholder="Event Title" />
+                                    <Form.Control required type="text" placeholder="Event Title" />
                                 </Col>
                             </Form.Group>
                             <Form.Row>
-                                <Form.Group as={Col} controlId="formGridEmail">
-                                    <Form.Group as={Row}>
+                                <Form.Group as={Col}>
+                                    <Form.Group as={Row} controlId="startDate">
                                         <Form.Label column sm="4">Start Date</Form.Label>
                                         <Col sm="8">
-                                            <Form.Control type="date" />
+                                            <Form.Control required type="date" />
                                         </Col>
                                     </Form.Group>
 
                                 </Form.Group>
 
-                                <Form.Group as={Col} controlId="formGridPassword">
-                                    <Form.Group as={Row}>
+                                <Form.Group as={Col}>
+                                    <Form.Group as={Row} controlId="endDate">
                                         <Form.Label column sm="4">End Date</Form.Label>
                                         <Col sm="8">
-                                            <Form.Control type="date" />
+                                            <Form.Control required type="date" />
                                         </Col>
                                     </Form.Group>
 
                                 </Form.Group>
                             </Form.Row>
                             <Form.Row>
-                                <Form.Group as={Col} controlId="formGridEmail">
-                                    <Form.Group as={Row}>
+                                <Form.Group as={Col} >
+                                    <Form.Group as={Row} controlId="beginHour">
                                         <Form.Label column sm="4">Begins</Form.Label>
                                         <Col sm="8">
                                             <Form.Control type="time" />
@@ -58,8 +77,8 @@ class EventModal extends Component {
 
                                 </Form.Group>
 
-                                <Form.Group as={Col} controlId="formGridPassword">
-                                    <Form.Group as={Row}>
+                                <Form.Group as={Col} >
+                                    <Form.Group as={Row} controlId="endHour">
                                         <Form.Label column sm="4">Ends</Form.Label>
                                         <Col sm="8">
                                             <Form.Control type="time" />
@@ -68,7 +87,7 @@ class EventModal extends Component {
 
                                 </Form.Group>
                             </Form.Row>
-                            <Form.Group as={Row} controlId="formPlaintextPeople">
+                            <Form.Group as={Row} controlId="people">
                                 <Form.Label column sm="2">
                                     People
                             </Form.Label>
@@ -76,7 +95,7 @@ class EventModal extends Component {
                                     <Form.Control type="text" />
                                 </Col>
                             </Form.Group>
-                            <Form.Group as={Row} controlId="formPlaintextLocation">
+                            <Form.Group as={Row} controlId="location">
                                 <Form.Label column sm="2">
                                     Location
                             </Form.Label>
@@ -84,7 +103,7 @@ class EventModal extends Component {
                                     <Form.Control type="text" />
                                 </Col>
                             </Form.Group>
-                            <Form.Group as={Row} controlId="formPlaintextDescription">
+                            <Form.Group as={Row} controlId="description">
                                 <Form.Label column sm="2">
                                     Description
                             </Form.Label>
