e59109f2610b126edf9576541084592a7aec3a64
Artan Muzhaqi <artanmuzhaqi@gmail.com>
2020-04-22 23:09:50 -0400

    view events by day

--- e-calendar/src/App.js
+++ e-calendar/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Calendar from './components/Calendar';
 import './main.scss';
 import Button from 'react-bootstrap/Button';
-import { Row, Container, Col } from 'react-bootstrap';
+import { Row, Container, Col, ListGroup } from 'react-bootstrap';
 import Modal from './components/EventModal';
 import * as moment from 'moment';
 
@@ -63,17 +63,19 @@ class App extends React.Component {
       <div className="App">
         <h1 align="center">eCalendar</h1>
         <Container fluid>
-          <Row>
-            <Col xs={12} lg={8}>
+          <Row className="main-row">
+            <Col xs={12} sm={10} md={10} lg={9} xl={6}>
               <Calendar changeDate={this.changeDate} events={this.state.events} />
             </Col>
-            <Col>
-              <h2>{days[this.state.date.getDay()]}</h2>
-              {/* The wrong date is being displayed might need to use moment.js or another library */}
-              <h1>{months[this.state.date.getMonth()]} {this.state.date.getDate()}</h1>
-              <ul>
-                {this.state.today ? this.state.today.map((event, i) => event && <li key={i}>{event}</li>) : ""}
-              </ul>
+            <Col className="events-container" md={10} lg={3}>
+              <div>
+                <h2>{days[this.state.date.getDay()]}</h2>
+                {/* The wrong date is being displayed might need to use moment.js or another library */}
+                <h1>{months[this.state.date.getMonth()]} {this.state.date.getDate()}</h1>
+              </div>
+              <ListGroup className="events-list" >
+                {this.state.today ? this.state.today.map((event, i) => event && <ListGroup.Item key={i}>{event}</ListGroup.Item>) : ""}
+              </ListGroup>
               <Button onClick={this.handleShow}>Add</Button>
               <Modal show={this.state.show} handleClose={this.handleClose} addEvent={this.addEvent} />
             </Col>
