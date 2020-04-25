d755821fb5c75cd00ae9b2e714094a99b9d9a00c
Artan Muzhaqi <artanmuzhaqi@gmail.com>
2020-04-24 21:26:08 -0400

    include the build folder

---
+++ docs/precache-manifest.04fd5e09a06cfdc6fea7613ccc81753c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0de6a3a8ee5cf8ff046079bd6a9b81e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d7c3d5aed14369d9538f",
+    "url": "/static/css/2.1d3b12e7.chunk.css"
+  },
+  {
+    "revision": "81ede7649b3cb4554395",
+    "url": "/static/css/main.fe5c35a8.chunk.css"
+  },
+  {
+    "revision": "d7c3d5aed14369d9538f",
+    "url": "/static/js/2.48fdda3b.chunk.js"
+  },
+  {
+    "revision": "139add4edced3e223256322761e04ae5",
+    "url": "/static/js/2.48fdda3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81ede7649b3cb4554395",
+    "url": "/static/js/main.f4ebd8f2.chunk.js"
+  },
+  {
+    "revision": "cbfa6760b28ced5d7489",
+    "url": "/static/js/runtime-main.91ba2500.js"
+  }
+]);
